89926e04e255d4e04aee858c9392d38ea08a6984
peng <pengx@alum.lehigh.edu>
2023-09-03 16:47:17 -0400

    backend authentication implemented

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-const products = require("./data/products.js");
 const productRoute = require("./routes/productRoute.js");
+const userRoutes = require("./routes/userRoutes.js");
 const express = require("express");
 const dotenv = require("dotenv");
+const cookie_parser = require("cookie-parser");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware.js");
 
 dotenv.config();
@@ -11,11 +12,20 @@ connectDB();
 const PORT = process.env.PORT || 8500;
 
 const app = express();
+
+// add body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// add cookie parser middleware
+app.use(cookie_parser());
+
 app.get("/", (req, res) => {
   res.send("Hi, there!");
 });
 
 app.use("/api/products", productRoute);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
