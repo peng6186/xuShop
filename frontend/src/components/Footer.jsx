ce73b22018a6489de55f506e39a10f854ca83a7b
peng <pengx@alum.lehigh.edu>
2023-09-17 21:55:19 -0400

    add github link

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,15 @@ const Footer = () => {
   const curYear = new Date().getFullYear();
   return (
     <div
-      className="mt-4 text-center text-base text-slate-400  h-16 w-full flex justify-center items-center
+      className="max-w-[75%] mx-auto flex items-center justify-between mt-4 text-center text-base text-slate-400  h-16 w-full
     "
     >
       <p>XuShop &copy; {curYear}</p>
+      <div className="cursor-pointer">
+        <a href="https://github.com/xusde" target="_blank">
+          <img src="/images/github.png" alt="github" />
+        </a>
+      </div>
     </div>
   );
 };
